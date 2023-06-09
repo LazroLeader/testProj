347b94f91725f5d76a41aaf3875ac47cc478f860
LazroLeader <l.giorgobiani@lider-bet.com>
2023-06-09 14:57:53 +0400

    Initial Commit

---
+++ src/test/java/com/gradleAxionSpringboot/testProj/TestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradleAxionSpringboot.testProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
